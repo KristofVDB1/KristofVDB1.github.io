5cae552d779de114c8165ec3fed74cf3d9058b50
KristofVDB1 <kristof.vandenbrouck@gmail.com>
2020-11-10 17:48:26 +0100

    Stlying changes, gridiamges, banner animation & potential imagedetail

--- main.js
+++ main.js
@@ -10,18 +10,20 @@ const mainNavLinks = document.querySelectorAll('nav ul li a');
 const langSelector = document.querySelector('.language-picker');
 document.addEventListener('scroll', function () {
   const fromTop = window.pageYOffset;
-  mainNavLinks.forEach(function (link) {
-    let section = document.querySelector(link.hash);
-
-    if (
-      section.offsetTop <= (fromTop + 80) &&
-      section.offsetTop + section.offsetHeight > (fromTop + 80)
-    ) {
-      link.classList.add("active");
-    } else {
-      link.classList.remove("active");
-    }
-  });
+  if (mainNavLinks.length > 1) {
+    mainNavLinks.forEach(function (link) {
+      let section = document.querySelector(link.hash);
+  
+      if (
+        section.offsetTop <= (fromTop + 80) &&
+        section.offsetTop + section.offsetHeight > (fromTop + 80)
+      ) {
+        link.classList.add("active");
+      } else {
+        link.classList.remove("active");
+      }
+    });
+  }
   let header = document.querySelector('header');
   let windowPosition = window.pageYOffset  > 0;
   if (scrollBool !== windowPosition) {
@@ -76,7 +78,82 @@ function translate() {
 
 document.getElementsByClassName('language__select')[0].addEventListener('change', function(e) {
   $("[data-localize]").localize("language", { language: e.target.value });
-})
+});
+
+// image gallery
+
+const elApp = document.querySelector("#app");
+
+const elImages = Array.from(document.querySelectorAll(".project-gallery-image"));
+
+const elDetail = document.querySelector(".detail");
+
+function flipImages(firstEl, lastEl, change) {
+  const firstRect = firstEl.getBoundingClientRect();
+
+  const lastRect = lastEl.getBoundingClientRect();
+
+  // INVERT
+  const deltaX = firstRect.left - lastRect.left;
+  const deltaY = firstRect.top - lastRect.top;
+  const deltaW = firstRect.width / lastRect.width;
+  const deltaH = firstRect.height / lastRect.height;
+
+  change();
+  lastEl.parentElement.dataset.flipping = true;
+
+  const animation = lastEl.animate([
+    {
+      transform: `translateX(${deltaX}px) translateY(${deltaY}px) scaleX(${deltaW}) scaleY(${deltaH})`
+    },
+    {
+      transform: 'none'
+    }
+  ], {
+    duration: 400, // milliseconds
+    easing: 'cubic-bezier(.2, 0, .3, 1)'
+  });
+
+  animation.onfinish = () => {
+    delete lastEl.parentElement.dataset.flipping;
+  }
+
+}
+
+elImages.forEach(figure => {
+
+  figure.addEventListener("click", () => {
+    const elImage = figure.querySelector('img');
+
+    elDetail.innerHTML = "";
+
+    const elClone = figure.cloneNode(true);
+    elDetail.appendChild(elClone);
+
+    const elCloneImage = elClone.querySelector('img');
+
+    flipImages(elImage, elCloneImage, ()=>{
+      elApp.dataset.state="detail";
+    });
+
+    function revert(){
+
+      flipImages(elCloneImage, elImage, ()=>{
+        elApp.dataset.state="gallery";
+        elDetail.removeEventListener('click',revert);
+        document.removeEventListener('scroll', revert);
+      });
+
+    }
+
+    document.addEventListener('scroll', revert);
+    elDetail.addEventListener('click',revert);
+
+  });
+});
+
+
+
 
 translate();
 navSlide();
